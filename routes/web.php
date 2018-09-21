1b899250494cacb2249b9c0e5c073f6bec276f2c
stupidly <stupidly@gmail.com>
2018-09-21 15:21:02 +0200

    Router fixed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
 use App\Controllers\HomeController;
+use Psr\Http\Message\{
+    ServerRequestInterface as Request,
+    ResponseInterface as Response
+};
 
-$app->get('/', HomeController::class . ':index');
+$app->get("/", HomeController::class . ":index");
+$app->get("/page1", function(Request $request, Response $response, $args){
+	return $this->view->render($response, "page1.twig", [
+            "text" => "Csoki",
+        ]);
+});
